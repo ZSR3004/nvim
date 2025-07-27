af4399c7401c5250f785f79bf6c1db6d8e7008d8
Ziyad Rahman <zrahman3004@gmail.com>
2025-07-27 11:55:03 -0500

    added bufferline

--- lua/plugins/file_nav.lua
+++ lua/plugins/file_nav.lua
@@ -519,4 +519,23 @@ return {
       })
     end,
   },
+  
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+
+      require("bufferline").setup({
+        options = {
+          mode = "buffers",
+          separator_style = "slant",
+        },
+      })
+
+      vim.keymap.set("n", "<S-l>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<S-h>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+    end,
+  }
+
 }
