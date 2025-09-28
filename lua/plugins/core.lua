74b30bb1306d4be29dbc751b4df2207c7dd6be95
Ziyad Rahman <zrahman3004@gmail.com>
2025-09-27 23:22:48 -0400

    commented out redundant luasnip import

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -73,33 +73,33 @@ return {
      dependencies = 'nvim-tree/nvim-web-devicons'
   },
 
-  { -- Luasnip
-    "L3MON4D3/LuaSnip",
-    version = "2.*",
-    build = "make install_jsregexp", -- optional, but recommended
-    config = function()
-      local ls = require("luasnip")
-      require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/LuaSnip/" })
-
-      -- Keymaps for expansion/jumping
-      vim.keymap.set({ "i", "s" }, "<Tab>", function()
-        if ls.expand_or_jumpable() then
-          return "<Plug>luasnip-expand-or-jump"
-
-        else
-          return "<Tab>"
-        end
-      end, { expr = true, silent = true })
-      vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
-
-        if ls.jumpable(-1) then
-          return "<Plug>luasnip-jump-prev"
-        else
-          return "<S-Tab>"
-        end
-      end, { expr = true, silent = true })
-    end,
-  },
+  -- { -- Luasnip
+  --   "L3MON4D3/LuaSnip",
+  --   version = "2.*",
+  --   build = "make install_jsregexp", -- optional, but recommended
+  --   config = function()
+  --     local ls = require("luasnip")
+  --     require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/LuaSnip/" })
+  --
+  --     -- Keymaps for expansion/jumping
+  --     vim.keymap.set({ "i", "s" }, "<Tab>", function()
+  --       if ls.expand_or_jumpable() then
+  --         return "<Plug>luasnip-expand-or-jump"
+  --
+  --       else
+  --         return "<Tab>"
+  --       end
+  --     end, { expr = true, silent = true })
+  --     vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+  --
+  --       if ls.jumpable(-1) then
+  --         return "<Plug>luasnip-jump-prev"
+  --       else
+  --         return "<S-Tab>"
+  --       end
+  --     end, { expr = true, silent = true })
+  --   end,
+  -- },
 
   { -- CMP
     "hrsh7th/nvim-cmp",
