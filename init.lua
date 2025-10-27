2db7ee455066299f2047a37cf05659566aa4ba51
Ziyad Rahman <zrahman3004@gmail.com>
2025-10-27 00:16:56 -0400

    Changed default tabsize to 2

--- init.lua
+++ init.lua
@@ -1,4 +1,14 @@
 require("config.lazy")
 
+-- Local Variables --
+local opt=vim.opt
+
+-- Tab Sizes --
+opt.tabstop=2
+opt.softtabstop=2
+opt.shiftwidth=4
+opt.expandtab=true
+
+-- Line Numbers --
 vim.wo.number = true
 vim.wo.relativenumber = true
