1540e7bff99ea1566ea74f7f8c68b619d7b5cb3d
Ziyad Rahman <zrahman3004@gmail.com>
2025-10-28 00:00:49 -0400

    added mason and mason lsp config

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,3 +1,22 @@
 return {
+  { -- LSP Config
+    "neovim/nvim-lspconfig",
+  },
 
+  { -- Mason
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+
+  { -- Mason LSP Config
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "pyright" }
+      })
+    end
+  },
 }
