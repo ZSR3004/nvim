edaf198265df601b4117bab4dc34baa3662faaf3
Ziyad Rahman <zrahman3004@gmail.com>
2025-11-13 10:42:18 -0500

    setup lsp, mason, and cmp

--- lua/plugins/lang_supp.lua
+++ lua/plugins/lang_supp.lua
@@ -1,14 +1,5 @@
 return { 
-  { -- CMP
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path"
-    },
-  },
-
+  
   { -- Autopairs
     "windwp/nvim-autopairs",
     event = "InsertEnter",
--- lua/plugins/lsp_conf.lua
+++ lua/plugins/lsp_conf.lua
@@ -1,22 +1,83 @@
 return {
-  { -- Mason
-    "mason-org/mason.nvim",
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = { ":TSUpdate", },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true,
+  },
+
+  {
+    "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
     end
   },
 
-  { -- Mason LSP Config
+  {
     "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
   },
+{
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
 
-  { -- Treesitter
-    "nvim-treesitter/nvim-treesitter",
-    branch = "main",
-    build = ":TSUpdate",
-  },
+    require("luasnip.loaders.from_vscode").lazy_load()
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+  end,
+},
 
-  { -- LSP Config
-    "neovim/nvim-lspconfig",
-  },
 }
