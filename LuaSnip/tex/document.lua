4eb0fab118fe164d4715285621e51be900e1c0b9
Ziyad Rahman <zrahman3004@gmail.com>
2025-11-15 10:02:13 -0500

    snippet for root file marker

---
+++ LuaSnip/tex/document.lua
@@ -0,0 +1,17 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local sn = ls.snippet_node
+
+return {
+  
+  -- Root File Marker
+  s("inp", {
+    t({"%! TEX root = "}),
+    i(1, "../main.tex")
+  })
+
+}
