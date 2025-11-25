7b0052d23b4d565082449e800e58b2940887022e
Ziyad Rahman <zrahman3004@gmail.com>
2025-11-25 13:52:55 -0500

    Added snippet for inline math mode

---
+++ LuaSnip/tex/environments.lua
@@ -0,0 +1,15 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local sn = ls.snippet_node
+
+return {
+
+  s("ii", {
+    i("$", 1, "$")
+  })
+
+}
