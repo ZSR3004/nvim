3334328e6728d62d16456cec1410eb3615cfbaf5
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-01 12:46:22 -0600

    Removed buffers; replaced with Telescope

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -4,11 +4,6 @@
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Close buffer" })
 
--- Buffer Navigation --
-vim.keymap.set("n", "<leader>w", ":bdelete<CR>", { desc = "Closes buffer" })
-vim.keymap.set("n", "<C-l>", ":bnext<CR>", { desc = "Move left a buffer" })
-vim.keymap.set("n", "<C-h>", ":bprevious<CR>", { desc = "Move right a buffer" })
-
 -- Treesitter --
 vim.keymap.set(
   "n", "<leader>d",
