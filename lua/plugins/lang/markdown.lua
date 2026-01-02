0e80f2500d97a5977ad298ddd6475f7db2ab9a12
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-02 10:57:14 -0600

    Added Markdown Links

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -5,4 +5,12 @@ return {
     ft = { "markdown" },
   },
 
+  { -- Markdown Links
+    'jghauser/follow-md-links.nvim',
+    ft = { "markdown" },
+    config = function()
+      vim.keymap.set('n', '<bs>', ':edit #<cr>', { silent = true })
+    end,
+  },
+
 }
