e07ac0fbbb9bbecc8c6042cff1b2861d73242993
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-02 13:16:37 -0600

    Added keybind for easy escape out of insert mode

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -3,6 +3,7 @@
 -- File IO and Writing --
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Close buffer" })
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape insert mode" })
 
 -- Buffer Navigation --
 vim.keymap.set("n", "<C-l>", ":bnext<CR>", { desc = "Move to next buffer" })
