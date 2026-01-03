19e1e08a1c38dda0ceb7f464dd3919904a5c3e62
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-03 11:19:36 -0600

    Enabled autocompleted snippets

--- lua/plugins/lang/luasnip.lua
+++ lua/plugins/lang/luasnip.lua
@@ -4,6 +4,12 @@ return {
     "L3MON4D3/LuaSnip",
     version = "v2.*",
     build = "make install_jsregexp",
+    opts = {
+      enabled_autosnippets = true,
+    },
+    config = function (_, opts)
+      require("luasnip").config.set_config(opts)
+    end
   },
 
 }
