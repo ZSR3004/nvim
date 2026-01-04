576cd6b1371fb7cee806566a6bf5053796d9d0b7
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-03 21:37:08 -0600

    Added keybind to remove highlights after searching

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -8,3 +8,4 @@ vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape insert mode" })
 -- Buffer Navigation --
 vim.keymap.set("n", "<C-l>", ":bnext<CR>", { desc = "Move to next buffer", silent = true })
 vim.keymap.set("n", "<C-h>", ":bprevious<CR>", { desc = "Move to previous buffer", silent = true })
+vim.keymap.set("n", "<leader>h", ":noh<CR>", { desc = "Removes highlights; mostly for after searching", silent = true })
