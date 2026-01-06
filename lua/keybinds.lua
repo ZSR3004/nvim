f1b274dc7bb167fffd01aafa2d9e9bb9259ce148
Ziyad Rahman <zrahman3004@gmail.com>
2026-01-06 01:26:07 -0600

    Silenced keybinds for writing to a file and exiting insert mode

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,9 +1,9 @@
 --- Keybinds ---
 
 -- File IO and Writing --
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file", silent = true })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Close buffer" })
-vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape insert mode" })
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape insert mode", silent = true })
 
 -- Buffer Navigation --
 vim.keymap.set("n", "<C-l>", ":bnext<CR>", { desc = "Move to next buffer", silent = true })
